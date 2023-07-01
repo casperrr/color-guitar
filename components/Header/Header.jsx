b28c75e731b5d1fe5b9baa3fdde96402dd3386b6
casperrr <casper.sebastien@gmail.com>
2023-07-01 15:27:52 +0100

    added link tag to the logo takes back to the root

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import styles from './Header.module.css';
+import Link from 'next/link';
 
 export default function Header(){
 
 
     return(
         <header className={styles.header_container}>
-            <h2 className={styles.header_title}>Guitar<span>Color</span></h2>
+            <Link href='/' style={{textDecoration: 'none'}}>
+                <h2 className={styles.header_title}>Guitar<span>Color</span></h2>
+            </Link>
         </header>
     );
 }
