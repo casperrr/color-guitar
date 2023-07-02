25ce7d5da886a41e13590a4d1e952f7f0a6f9a83
casperrr <casper.sebastien@gmail.com>
2023-07-02 12:52:58 +0100

    last commit before push

--- components/Controls/Controls.jsx
+++ components/Controls/Controls.jsx
@@ -1,10 +1,13 @@
+import Dropdown from '../Inputs/Dropdown/Dropdown';
 import styles from './Controls.module.css';
 
 export default function Controls(){
 
     return(
         <div className={styles.controls_container}>
-            
+            <Dropdown name={"This is a button"}>
+                <p>Here is some text that u cant see</p>
+            </Dropdown>
         </div>
     );
 }
--- components/Inputs/Dropdown/Dropdown.jsx
+++ components/Inputs/Dropdown/Dropdown.jsx
@@ -1,10 +1,78 @@
+"use client";
+import React, {useState} from "react";
+import styles from './Dropdown.module.css';
 
 
-const Dropdown = () => {
+
+// const Dropdown = () => {
+
+//     const options = [
+//         {value: '1', label: 'weee'},
+//         {value: "2", label: "pee"}
+//     ];
+
+//     const [value, setValue] = useState(options[0].value);
+
+//     const handleChange = (event) => {
+//         setValue(event.target.value);
+//     }
+
+//     return (
+//         <div className={styles.dropdown_container}>
+//             <h4>Scale</h4>
+//             <select name="test" value={value} onChange={handleChange} className={styles.dropdownButton}>
+//                 {options.map((option) => (
+//                     <option value={option.value}>{option.label}</option>
+//                 ))}
+
+//             </select>
+//             <h3>Value = {value}</h3>
+//         </div>
+//     );
+// }
+
+
+
+let items = [
+    'toke','toked','toker','haz clarke','tokest'
+];
+
+
+const Dropdown = (props) => {
+    const [open, setOpen] = useState(false);
+
+    return(
+        <div className={styles.dropdown}>
+            <a className={styles.dropdownBtn} 
+            onClick={() => setOpen(!open)}>
+                <p >
+                    {props.name}
+                </p>
+                <span>+</span>
+            </a>
+            {open && <DropdownMenu items={items}/>}
+        </div>
+    )
+}
+
+function DropdownMenu(props){
+
+    let items = props.items;
     
-  return (
-    <div>Dropdown</div>
-  )
+    function DropdownItem(props){
+        return(
+            <a className={styles.DropdownItem}>
+                {props.children}
+            </a>
+        )
+    }
+
+    return(
+        <div className={styles.DropdownMenu}>
+            {items.map((item) => <DropdownItem>{item}</DropdownItem>)}
+            
+        </div>
+    )
 }
 
 export default Dropdown
