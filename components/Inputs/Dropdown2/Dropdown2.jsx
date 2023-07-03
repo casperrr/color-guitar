cf15b42bddaa8265268b3b9f5e6add53a0ccf6f5
casperrr <casper.sebastien@gmail.com>
2023-07-03 20:18:56 +0100

    rearanged the code for dropdown

--- components/Inputs/Dropdown2/Dropdown2.jsx
+++ components/Inputs/Dropdown2/Dropdown2.jsx
@@ -7,48 +7,51 @@ let items = ['item 1', 'ohhhhh', 'item 3', 'toker'];
 export default function Dropdown(props) {
 
     const [open, setOpen] = useState(false);
+    const [selected, setSelected] = useState();
 
 
     function handleClick(){
         setOpen(!open);
         // console.log(document.getElementsByClassName(styles.Menu));
-        let menu = document.getElementsByClassName(styles.Menu)[0];
-        let translate = menu.clientHeight;
-        translate = open ? 0 : translate;
-        menu.style.transform = `translateY(${translate}px)`;
+        openClose(open)
     }
 
-    return(
-        <div className={styles.dropdown}>
-            <button className={styles.DropdownBtn} onClick={() => handleClick()}>{props.title}
-            {open ? <span>-</span>: <span>+</span>}</button>
-            <Menu/>
-        </div>
-    )
-}
 
-function Menu(){
-
-    
-
-    
     function MenuItem(props){
         
+        function handleSelect(e){
+            let target = e.target.textContent;
+            console.log(target)
+            handleClick();
+        }
+
         return(
-            <li className={styles.MenuItem}>
+            <li className={styles.MenuItem} onClick={(e) => handleSelect(e)}>
                 <a>{props.children}</a>
                 <div></div>
             </li>
         )
     }
-
+    
 
     return(
-        <div className={styles.menuHider}>
-            <ul className={styles.Menu}>
-                <li className={styles.gap}></li>
-                {items.map((item) => <MenuItem>{ item }</MenuItem>)}
-            </ul>
+        <div className={styles.dropdown}>
+            <button className={styles.DropdownBtn} onClick={() => handleClick()}>{props.title}
+            {open ? <span>-</span>: <span>+</span>}</button>
+            <div className={styles.menuHider}>
+                <ul className={styles.Menu}>
+                    <li className={styles.gap}></li>
+                    {items.map((item) => <MenuItem>{ item }</MenuItem>)}
+                </ul>
+            </div>
         </div>
     )
+}
+
+
+function openClose(open){
+    let menu = document.getElementsByClassName(styles.Menu)[0];
+    let translate = menu.clientHeight;
+    translate = open ? 0 : translate;
+    menu.style.transform = `translateY(${translate}px)`;
 }
